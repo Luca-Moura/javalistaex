8e40690c8ffbbcdfde1680e62bccf7988a230353
Luca <luca.mm01@gmail.com>
2022-06-09 10:09:50 -0300

    Ex3 Ok

---
+++ src/javalistaex/Ex3.java
@@ -0,0 +1,19 @@
+package javalistaex;
+import java.util.Scanner;
+public class Ex3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		int d;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+	System.out.println("O resultado de (a x b - c x d) é " + (a*b-c*d));
+
+	}
+
+}
