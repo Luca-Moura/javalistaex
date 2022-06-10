bc0d54a9de89533d662bfa35668bc4e620eb0f30
Luca <luca.mm01@gmail.com>
2022-06-10 20:32:17 -0300

    Ex1L2 ok

---
+++ listas/src/Ex1L2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex1L2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x;
+		System.out.println("Insira a senha ");
+		x = sc.nextInt();
+		if (x == 2002) System.out.println("Senha correta!");
+		else System.out.println("Senha errada!");
+		
+	}
+}
